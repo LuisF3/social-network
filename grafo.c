7cf23cb74c535ee6eddb9a8d33f913abaaa14706
Luís Felipe Chaves <pi.luis.f3@usp.br>
2019-06-04 16:15:21 -0300

    Tad grafo e desenho do logo criado. Ainda não testado

---
+++ grafo.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "grafo.h"
+
+/* -- Grafo -- */
+
+struct _grafo{
+    usuario *cabeca, *ultimo;
+    int qtd_usuarios;
+};
+
+struct _usuario_no{
+    int id;
+    int idade;
+    char *nome;
+    char *cidade;
+    char *genero_filme;
+    char *cor_favorita;
+    char *time;
+
+    lista *amizades;
+    usuario *proximo;
+};
+
+int ultimo_id = 1;
+
+grafo *grafoCriar(){
+    grafo *g = (grafo*)malloc(sizeof(grafo));
+    if(!g) return NULL;
+
+    g->cabeca = (usuario *)malloc(sizeof(usuario));
+    if(!g->cabeca){
+        free(g);
+        return NULL;
+    }
+
+    g->ultimo = NULL;
+    g->cabeca->proximo = NULL;
+    g->qtd_usuarios = 0;
+
+    return g;
+}
+
+int grafoVazio(grafo *g){
+    return g->qtd_usuarios == 0;
+}
+
+void grafoInserirFim(grafo *g, int idade, char *nome, char *cidade, char *genero_filme, char *cor_favorita, char *time){
+    usuario *novo = (usuario *) malloc(sizeof(usuario));
+    novo->proximo = NULL;
+
+    // Atribuição das variáveis de um usuário
+    novo->id = ultimo_id++;
+    novo->idade = idade;
+    strcpy(novo->nome, nome);
+    strcpy(novo->cidade, cidade);
+    strcpy(novo->genero_filme, genero_filme);
+    strcpy(novo->cor_favorita, cor_favorita);
+    strcpy(novo->time, time);
+
+    if(grafoVazio(g)) g->cabeca->proximo = novo;
+    else g->ultimo->proximo = novo;
+    g->ultimo = novo;
+
+    g->qtd_usuarios++;
+}
+
+void grafoRemoverBusca_id(grafo *g, int id){
+    if(grafoVazio(g)) return;
+    usuario *remover = g->cabeca;
+    while(remover->proximo != NULL && remover->proximo->id < id)
+        remover = remover->proximo;
+    if(remover->proximo == NULL || remover->proximo->id > id) return;
+    usuario *aux = remover->proximo;
+    remover->proximo = remover->proximo->proximo;
+    if(aux == g->ultimo) free(g->ultimo);
+    free(aux);
+    g->qtd_usuarios--;
+}
+
+void grafoRemoverInicio(grafo *g){
+    if(grafoVazio(g)) return;
+    usuario *remover = g->cabeca->proximo;
+    g->cabeca->proximo = remover->proximo;
+    if(g->qtd_usuarios == 1) g->ultimo = NULL;
+    free(remover);
+    g->qtd_usuarios--;
+}
+
+void grafoApagar(grafo *g){
+    if(!g) return;
+    while(!grafoVazio(g))
+        grafoRemoverInicio(g);
+    free(g->cabeca);
+    free(g);
+}
+
+/* -- Lista -- */
+
+struct LISTA{
+    int tamanho;
+    nohLista *cabeca;
+    nohLista *fim;
+};
+
+struct NOHLISTA{
+    int id;
+    usuario *amigo;
+
+    nohLista *proximo;
+};
+
+lista *listaCriar(){
+    lista *l = (lista*)malloc(sizeof(lista));
+    if(!l) return NULL;
+
+    l->cabeca = (nohLista*)malloc(sizeof(nohLista));
+    if(!l->cabeca){
+        free(l);
+        return NULL;
+    }
+
+    l->cabeca->proximo = NULL;
+    l->tamanho = 0;
+
+    return l;
+}
+
+int listaVazia(lista *l){
+    return l->tamanho == 0;
+}
+
+void listaInserirFim(lista *l, int id, usuario *amigo){
+    nohLista *novo = (nohLista*)malloc(sizeof(nohLista));
+    novo->proximo = NULL;
+    
+    novo->id = id;
+    novo->amigo = amigo;
+
+    if(listaVazia(l)) l->cabeca->proximo = novo;
+    else l->fim->proximo = novo;
+    l->fim = novo;
+
+    l->tamanho++;
+}
+
+void listaInserirOrdenado(lista *l, int id, usuario* amigo){
+    if(listaVazia(l) || id > l->fim->id)
+        listaInserirFim(l,id, amigo);
+
+    nohLista *novo = (nohLista*)malloc(sizeof(nohLista));
+    novo->proximo = NULL;
+    
+    novo->id = id;
+    novo->amigo = amigo;
+
+    nohLista *p = l->cabeca;
+
+    while(p->proximo != NULL && p->proximo->id < novo->proximo->id)
+        p = p->proximo;
+    novo->proximo = p->proximo;
+    p->proximo = novo;
+    l->tamanho++;
+}
+
+void listaRemoverInicio(lista *l){
+    if(listaVazia(l)) return;
+    nohLista *remover = l->cabeca->proximo;
+    l->cabeca->proximo = remover->proximo;
+    if(l->tamanho == 1) l->fim = NULL;
+    free(remover);
+    l->tamanho--;
+}
+
+void listaRemoverBusca_id (lista *l, int id, usuario *amigo){
+    if(listaVazia(l)) return;
+    nohLista *remover = l->cabeca;
+    while(remover->proximo != NULL && remover->proximo->id < id)
+        remover = remover->proximo;
+    if(remover->proximo == NULL || remover->proximo->id > id) return;
+    nohLista *aux = remover->proximo;
+    remover->proximo = remover->proximo->proximo;
+    if(aux == l->fim) free(l->fim);
+    free(aux);
+    l->tamanho--;
+}
+
+void listaApagar(lista *l){
+    if(!l) return;
+    while(!listaVazia(l))
+        listaRemoverInicio(l);
+    free(l->cabeca);
+    free(l);
+}
---
+++ grafo.h
@@ -0,0 +1,69 @@
+#ifndef H_GRAFO
+#define H_GRAFO
+
+#define true 1
+#define false 0
+#define erro -1
+typedef int bool;
+
+typedef struct _grafo grafo;
+typedef struct _usuario_no usuario;
+
+grafo *grafo_criar();
+void grafo_inserir();
+void grafo_apagar();
+
+typedef struct LISTA lista;
+typedef struct NOHLISTA nohLista;
+
+/*
+* Aloca dinamicamente e inicializa a lista.
+* @return l: ponteiro da lista alocada dinamicamente com seus parâmetros inicializados.
+* Caso ocorra algum erro durante a alocação, o ponteiro terá valor nulo.
+*/
+lista *listaCriar();
+
+/*
+* Insere ordenadamente na lista de acordo com o conteúdo.
+* @param id: conteúdo do novo nó
+* @param usuario: conteúdo do novo nó
+* @param l: ponteiro da lista em que o nó será posicionado
+*/
+void listaInserirOrdenado(lista *l, int id, usuario *amigo);
+
+/*
+* Insere no fim da lista.
+* @param id: conteúdo do novo nó
+* @param usuario: conteúdo do novo nó
+* @param l: ponteiro da lista em que o nó será posicionado
+*/
+void listaInserirFim(lista *l, int id, usuario *amigo);
+
+/*
+* Remove o nó inicial da lista e retorna o seu conteúdo.
+* @param l: ponteiro da lista que terá seu componente desalocado 
+*/
+void listaRemoverInicio(lista *l);
+
+/*
+* Remove o elemento cujas especificações sejam iguais aos valores de comparação
+* @param l: lista em que será efetuada a remoção
+* @param chaveveBusca: valor de comparação para remover
+* @param usuario: valor de comparação para remover
+*/
+void listaRemoverBusca(lista *l, int id, usuario *amigo);
+
+/*
+* Indica se a lista possui algum elemento nela.
+* @param l: ponteiro da lista de verificação
+* @return int: 0 (lista não vazia) - 1 (lista vazia)
+*/
+int listaVazia(lista *l);
+
+/*
+* Exclui todos os elementos da lista, bem como desaloca a lista e o nó cabeça.
+* @param l: ponteiro da lista que será apagada
+*/
+void listaApagar(lista *l);
+
+#endif
