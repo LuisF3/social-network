55f2b6b4794b7d021bf1b01192c0c0af725d5053
Victor Akihito Kamada Tomita <USUARIOS+10692082@6-304-15.grad.usricmc.icmc.usp.br>
2019-06-16 14:37:45 -0300

    cores adicionadas no csv de forma legivel

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv("cidades.csv")
+df = pd.read_csv("cores.csv")
 
 columns = df.columns.tolist()
-print(columns)
+df.drop('Name_under', axis = 1, inplace = True)
+df.drop('Number', axis = 1, inplace = True)
 
-columns = [columns[1] , columns[0] , columns[2] , columns[3]]
-# df.sort_values('2')
 
-df = df[columns]
-df.to_csv('cidades.csv', index = False)
+print(df.head())
+
+
+# columns = [columns[1] , columns[0] , columns[2] , columns[3]]
+# df.sort_values(by=['Brand'], inplace=True)
+
+# df = df[columns]
+df.to_csv('cores.csv', index = False)
